4b00f7e66f752e4bc505b5d2574787e61d0516d2
teresalee1231 <69709178+teresalee1231@users.noreply.github.com>
2022-03-07 14:02:10 -0800

    updated run.sh, added run_bbr, updated graphs

--- run_bbr.sh
+++ run_bbr.sh
@@ -12,14 +12,14 @@ delay=5
 iperf_port=5001
 
 for qsize in 20 100; do
-    dir=bb-q$qsize
+    dir=bb-bbr-q$qsize
 
     # TODO: Run bufferbloat.py here...
-    python3 bufferbloat.py -d=$dir -t=$time -b=$bwnet --delay=$delay --maxq=$qsize
+    python3 bufferbloat.py -d=$dir -t=$time -b=$bwnet --delay=$delay --maxq=$qsize --cong bbr
 
     # TODO: Ensure the input file names match the ones you use in
     # bufferbloat.py script.  Also ensure the plot file names match
     # the required naming convention when submitting your tarball.
-    python3 plot_queue.py -f $dir/q.txt -o reno-buffer-q$qsize.png
-    python3 plot_ping.py -f $dir/ping.txt -o reno-rtt-q$qsize.png
+    python3 plot_queue.py -f $dir/q.txt -o bbr-buffer-q$qsize.png
+    python3 plot_ping.py -f $dir/ping.txt -o bbr-rtt-q$qsize.png
 done
